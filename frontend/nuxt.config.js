a3dd5a568ddc2f55159bef0b1c10c9de72f6b1f4
Phil Efstathiou <phil.efstat@gmail.com>
2020-04-22 17:57:26 +0100

    remove husky and stylelint

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -33,9 +33,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
