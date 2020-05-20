c72cb247a17e5d24a697075927b3dfa41e43b807
Sophie Mann <sklm@live.co.uk>
2020-05-20 12:03:40 +0100

    Use cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4000", "http://localhost:3000"],
+  })
+);
 
 app.use("/", require("./routes/routes"));
 
